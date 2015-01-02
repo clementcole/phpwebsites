aa1e1768406ed6243e962c7599c8e42e522fb8dd
Clement <clementacole75@gmail.com>
2015-01-01 20:04:47 -0600

    first commitment

---
+++ Pastor_Charles/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html> <!-- The new doctype -->
+<html>
+    <head>
+    
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+        <title>Welcome to flame of fire healing &amp; </title>
+        
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        
+        <!-- Internet Explorer HTML5 enabling code: -->
+        
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        
+        <style type="text/css">
+        .clear {
+          zoom: 1;
+          display: block;
+        }
+        </style>
+
+        
+        <![endif]-->
+        
+    </head>
+   
+    <body>
+    	
+    	<section id="page"> <!-- Defining the #page section with the section tag -->
+    
+            <header> <!-- Defining the header section of the page with the appropriate tag -->
+            
+                <hgroup>
+                    <h2 align="center">FLAME OF FIRE HEALING</h2>
+                    <h2 align="center">Church's Slogan goes here!!</h2>
+                </hgroup>
+                                
+                <nav class="clear"> <!-- The nav link semantically marks your main site navigation -->
+                    <ul>
+                        <li><a href="#article1">Photos</a></li>
+                        <li><a href="#article2">Videos</a></li>
+                        <li><a href="#article3">Testimonials</a></li>
+			<li><a href="#article4">Visions</a></li>
+			<li><a href="#article5">Blogs</a></li>
+			<!------   Link to social media sites   -->
+			<div id="share-buttons">
+						<!--facebook -->
+						<a href="https://www.facebook.com/pages/Flame-Of-Fire-Healing-Center-Ministries-International/1388547254733493?pnref=lhc" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" /></a>
+						<!-- Twitter -->
+						<a href="http://twitter.com" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a>
+ 						<!-- Google+ -->
+						<a href="https://plus.google.com/" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/google.png" alt="Google" /></a>
+						<!-- Pinterest -->
+						<a href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());"><img src="http://www.simplesharebuttons.com/images/somacro/pinterest.png" alt="Pinterest" /></a>
+ 						<!-- StumbleUpon-->
+						<a href="http://www.stumbleupon.com/submit?url=http://www.simplesharebuttons.com&title=Simple Share Buttons" target="_blank"><img src="http://www.simplesharebuttons.com/images/somacro/stumbleupon.png" alt="StumbleUpon" /></a>
+ 
+						<!-- Email -->
+						<a href="mailto:?Subject=Simple Share Buttons&Body=I%20saw%20this%20and%20thought%20of%20you!%20 http://www.simplesharebuttons.com"><img src="http://www.simplesharebuttons.com/images/somacro/email.png" alt="Email" /></a>
+						<!-- Youtube -->
+						<a href="http://youtube.com" target="_blank"><img src="youtube.png"/></a>
+				<style type="text/css">
+					#share-buttons img{
+					width: 35px;
+					padding: 5px;
+					border: 0;
+					box-shadow: 0;
+					display: inline;
+					}
+				</style>
+			</div>
+                    </ul>
+                </nav>
+            	
+            </header>
+            
+            <section id="articles"> <!-- A new section with the articles -->
+
+				<!-- Article 1 start -->
+
+                <div class="line"></div>  <!-- Dividing line -->
+                
+                <article id="article1"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
+                    <h2>PHOTOS</h2>
+                    
+                    <div class="line"></div>
+                    
+                    <div class="articleBody clear">
+                    
+                    	<figure>
+	                    <a href="assets/index1.html"><img class="bottom" src="pix-3.jpg"   width="620" height="340"  id="pic1"/>< img class="top"  src ="pix-2.jpg"  id="pic1" width="620" height="340" id="pic2"/></a>
+					
+					
+                        </figure>
+                        <p>&#10057; 		&#10057; 		&#10057;  	 	&#10057;   		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;</p>
+                        <p>&#10057;								PHOTO BOOTH								&#10057;</p>
+        		<p>&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;</p>
+                    </div>
+                </article>
+                
+				<!-- Article 1 end -->
+
+
+				<!-- Article 2 start -->
+
+                <div class="line"></div>
+                
+                <article id="article2">
+                    <h2>VIDEOS</h2>
+                    
+
+                    
+                    <div class="articleBody clear">
+                    	<figure>
+	                    	<a href="http://tutorialzine.com/2010/01/sweet-tabs-jquery-ajax-css/"><img src="http://tutorialzine.com/img/featured/633.jpg" width="620" height="340" /></a>
+                        </figure>
+                        
+                        <p>&#10057; 		&#10057; 		&#10057;  	 	&#10057;   		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;</p>
+                        <p>&#10057;								VIDEOS                                                           							&#10057;</p>
+        		<p>&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;</p>
+                    </div>
+                </article>
+                
+				<!-- Article 2 end -->
+
+				<!-- Article 3 start -->
+
+                <div class="line"></div>
+                
+                <article id="article3">
+                    <h2>TESTIMONIALS</h2>
+                    
+                    <div class="line"></div>
+                    
+                    <div class="articleBody clear">
+                    	<figure>
+	                    	<a href="http://tutorialzine.com/2010/01/halftone-navigation-menu-jquery-css/"><img src="http://tutorialzine.com/img/featured/610.jpg" width="620" height="340" /></a>
+                        </figure>
+                        
+                        <p>&#10057; 		&#10057; 		&#10057;  	 	&#10057;   		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;</p>
+                        <p>&#10057;								TESTIMONIALS                                                             						&#10057;</p>
+        		<p>&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;		&#10057;</p>
+                    </div>
+                </article>
+                
+				<!-- Article 3 end -->
+				
+				<!-- Article 4 start -->
+		<div class="line"></div>
+		<article id="article4">
+		<h2>VISION</h2>
+			<div class="line"></div>
+			<div class="articleBody clear">
+				<figure>
+					<a href="http://tutorialzine.com/2010/01/halftone-navigation-menu-jquery-css/"><img src="http://tutorialzine.com/img/featured/610.jpg" width="620" height="340" /></a>
+				</figure>
+			 <p>&#10057;             &#10057;             &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;</p>
+                         <p>&#10057;                                                                         VISION                                                                                                                           &#10057;</p>
+                         <p>&#10057;             &#10057;             &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                                        &#10057;</p>
+
+		</article>
+				<!-- Article 4 end -->
+
+				<!-- Article 5 start -->
+                  <div class="line"></div>
+                 <article id="article5">
+                 <h2>BLOGS</h2>
+                         <div class="line"></div>
+                         <div class="articleBody clear">
+                                 <figure>
+                                         <a href="http://tutorialzine.com/2010/01/halftone-navigation-menu-jquery-css/"><img src="http://tutorialzine.com/img/featured/610.jpg" width="620" height="340" /></a>
+                                 </figure>
+                          <p>&#10057;             &#10057;             &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;</p>
+                          <p>&#10057;                                                             BLOGS                                                                                                                                        &#10057;</p>
+                          <p>&#10057;             &#10057;             &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                &#10057;                                        &#10057;</p>
+                                 
+                 </article>      
+
+
+				<!-- Article 5 end -->
+		
+
+            </section>
+
+        <footer> <!-- Marking the footer section -->
+
+           <div class="line"></div>
+           
+           <p>AClements Enterprise</p> <!-- Change the copyright notice -->
+           
+           <a href="#" class="up">Go UP</a>
+           <a href="http:facebook.com" class="CodeJinxEnterprise est(2014)"></a>
+           
+
+        </footer>
+            
+		</section> <!-- Closing the #page section -->
+        
+        <!-- JavaScript Includes -->
+ 	<script src="script2.js"></script>       
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+        <script src="jquery.scrollTo-1.4.2/jquery.scrollTo-min.js"></script>
+	<script src="http://code.jquery.com/jquery-1.4.4.min.js" type="text/javascript"></script>
+        <script src="script.js"></script>
+    </body>
+</html>
